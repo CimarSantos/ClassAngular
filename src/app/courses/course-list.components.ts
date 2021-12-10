c0547950c702f7a3bdea780f20b14e9c52f8e298
Francimar <francimarlsantos@outlook.com>
2021-12-10 09:24:22 -0300

    Lista de Cursos

---
+++ src/app/courses/course-list.components.ts
@@ -0,0 +1,32 @@
+import { CourseService } from './course.service';
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+
+
+@Component ({
+
+
+    selector: 'app-course-list',
+    templateUrl: './course-list.components.html'
+
+})
+
+export class CourseListComponent implements OnInit{
+
+  courses: Course[] = [];
+
+  constructor (private courseService: CourseService) {
+
+
+
+  }
+
+  ngOnInit(): void {
+    this.courses = this.courseService.retrieveAll();
+
+  }
+
+
+}
+
